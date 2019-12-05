47f290b71c42302224c9231f68cc7cb0fe6b0b4c
a <a@adeMacBook-Pro-2.local>
2019-12-05 14:24:50 +0800

    -a

---
+++ Example/Pods/Target Support Files/QXCategoryKit/QXCategoryKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QXCategoryKit : NSObject
+@end
+@implementation PodsDummy_QXCategoryKit
+@end
---
+++ Example/Pods/Target Support Files/QXCategoryKit/QXCategoryKit-umbrella.h
@@ -0,0 +1,23 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSObject+extend.h"
+#import "NSObject+YCat.h"
+#import "UIButton+BorderCornerRadius.h"
+#import "UIColor+YCat.h"
+#import "UIView+CornerRadius.h"
+#import "UIView+DKBHelper.h"
+#import "UIViewController+YCat.h"
+
+FOUNDATION_EXPORT double QXCategoryKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char QXCategoryKitVersionString[];
+
